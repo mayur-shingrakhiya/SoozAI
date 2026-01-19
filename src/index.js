444c1458aabdea68cc2bb9b3aa46bcd0d37350c8
mayur-shingrakhiya <53403282+mayur-shingrakhiya@users.noreply.github.com>
2026-01-19 17:42:24 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -10,16 +10,4 @@ root.render(
   </React.StrictMode>
 );
 
-// Register Service Worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/service-worker.js')
-      .then((registration) => {
-        console.log('✅ SW registered successfully:', registration);
-      })
-      .catch((error) => {
-        console.log('❌ SW registration failed:', error);
-      });
-  });
-}
+serviceWorkerRegistration.register();
